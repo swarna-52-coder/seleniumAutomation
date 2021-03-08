112d84835b7078f1150446b0fc40316bb6fee345
swarna-52-coder <80219177+swarna-52-coder@users.noreply.github.com>
2021-03-07 19:31:09 -0500

    test
    name swarna

---
+++ src/main/java/com/muralitechnologies/lsel/SeleniumAutomationfw/App.java
@@ -0,0 +1,13 @@
+package com.muralitechnologies.lsel.SeleniumAutomationfw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
